2786db2269e886ea3f599b3c7c1201fddbb700c9
vishalkale9 <vishalkale1367@gmail.com>
2026-06-29 23:09:51 +0530

    navbar fix

--- supplychain/src/App.jsx
+++ supplychain/src/App.jsx
@@ -1,20 +1,11 @@
-import { Routes, Route } from "react-router-dom";
 import Navbar from "./layouts/Navbar";
 import Hero from "./components/Hero";
-import LoginForm from "./pages/LoginForm";
-import SignupForm from "./pages/SignupForm";
 
 const App = () => {
   return (
     <div className="bg-gray-50 min-h-screen font-sans">
       <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-        </Routes>
-      </main>
+      <Hero />
     </div>
   );
 };
